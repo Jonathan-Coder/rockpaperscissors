12310f73b8d95b78677a62fcc2c6dcff69fe22af
Glitch (rps-operation) <none>
2021-01-26 21:55:01 +0000

    Created Player choice

--- js/script.js
+++ js/script.js
@@ -4,6 +4,32 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+var cpuChoice = [""];
+var playerChoice = "";
+var wins = 0;
 
+function elem(str){
+  return document.querySelector(str);
+}
 
+function player(choice){
+  if(choice === "rock"){
+    elem("#userChoice").innerText = choice;
+  } else if(choice=== "scissors"){
+    elem("#userChoice").innerText = choice;
+  } else if(choice === "paper"){
+    elem("#userChoice").innerText = choice;
+  } else {
+    alert("Invlaid Choice: Please pick Rock, Paper or Scissors.");
+  }
+}
 
+function cpu(){
+  var num = Math.random() * 3;
+  
+}
+
+elem("#shoot").addEventListener("click", function(){
+  playerChoice = elem("#input").value.toLowerCase();
+  player(playerChoice);
+});
